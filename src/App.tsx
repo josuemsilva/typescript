ab0301dd8d08fa33c644519f84d8fd917cc52eef
JooJC <joojmoutinho45@gmail.com>
2023-10-16 16:42:18 -0300

    aprendendo typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Button from './Button'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button text='teste'/>
+      </div>
+    </>
+  )
+}
+
+export default App
